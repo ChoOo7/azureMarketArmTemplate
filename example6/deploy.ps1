846d92a0aa69f81f3de74748cf0bec7026def420
ChoOo7 <github@simon-minotto.com>
2015-09-01 17:38:00 +0200

    ajout divers

--- example6/deploy.ps1
+++ example6/deploy.ps1
@@ -10,8 +10,8 @@ $theDeploymentName = $("testAzureRessource"+$exempleName+"DN")
 $templateFile = "azuredeploy.json"
 $templateParametersFile = "azuredeploy.parameters.json"
     
-#Switch-AzureMode AzureResourceManager
+Switch-AzureMode AzureResourceManager
 #New-AzureResourceGroup -Location $theLocation -Name $theName -DeploymentName $theDeploymentName -TemplateFile $templateFile -TemplateParameterFile $templateParametersFile -appName $appName
-New-AzureResourceGroup -Location $theLocation -Name $theName -DeploymentName $theDeploymentName -TemplateFile $templateFile -appName $appName -Verbose -Debug
+New-AzureResourceGroup -Location $theLocation -Name $theName -DeploymentName $theDeploymentName -TemplateFile $templateFile -appName $appName -Verbose
 
 
