d3b869506cf712df108f705ef534b91e49ef9354
ChoOo7 <github@simon-minotto.com>
2015-09-01 12:08:36 +0200

    ajout example4

---
+++ example4/deploy.ps1
@@ -0,0 +1,15 @@
+$exempleName = "example41"
+
+
+#Copied from https://github.com/Azure/azure-quickstart-templates/tree/master/201-2-vms-loadbalancer-natrules
+
+$theLocation = "West Europe"
+$theName = $("testAzureRessource"+$exempleName+"RG")
+$theDeploymentName = $("testAzureRessource"+$exempleName+"DN")
+$templateFile = "azuredeploy.json"
+$templateParametersFile = "azuredeploy.parameters.json"
+    
+#Switch-AzureMode AzureResourceManager
+New-AzureResourceGroup -Location $theLocation -Name $theName -DeploymentName $theDeploymentName -TemplateFile $templateFile -TemplateParameterFile $templateParametersFile
+
+
