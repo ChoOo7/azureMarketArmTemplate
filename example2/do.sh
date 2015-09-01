3ecf849eec50d6d98d6000e8e5bc399184bec1bf
ChoOo7 <github@simon-minotto.com>
2015-09-01 03:13:33 +0200

    modification du script

--- example2/do.sh
+++ example2/do.sh
@@ -2,3 +2,13 @@
 
 date >> /tmp/date
 date >> /root/date
+
+date >> /root/provisioning
+echo "Arg1 : " >> /root/provisioning
+echo $1  >> /root/provisioning
+
+echo "Arg2 : " >> /root/provisioning
+echo $2  >> /root/provisioning
+
+echo "Arg2 : " >> /root/provisioning
+echo $2  >> /root/provisioning
