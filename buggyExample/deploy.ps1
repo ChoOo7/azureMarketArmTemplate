d078faa355a0be08962559adb089ecd93a060b81
ChoOo7 <github@simon-minotto.com>
2015-09-02 09:29:58 +0200

    adding buggy example for support

---
+++ buggyExample/deploy.ps1
@@ -0,0 +1,11 @@
+$exempleName = "buggyexample"
+
+$theLocation = "West Europe"
+$theName = $("testAzureRessource"+$exempleName+"RG")
+$theDeploymentName = $("testAzureRessource"+$exempleName+"DN")
+$templateFile = "azuredeploy.json"
+
+#Switch-AzureMode AzureResourceManager
+New-AzureResourceGroup -Location $theLocation -Name $theName -DeploymentName $theDeploymentName -TemplateFile $templateFile
+
+
